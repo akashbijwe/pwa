4503a8ca5baf5dd078b3616e1a6629eea5bc97e0
Akash Bijwe <mr.akashbijwe@gmail.com>
2018-07-03 13:30:04 +0530

    4th commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -75,32 +75,3 @@ self.addEventListener('fetch',(event) => {
 //         })
 //    )
 //   }) 
-
-//For Installation
-let installPromptEvent;
-
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome <= 67 from automatically showing the prompt
-  event.preventDefault();
-  // Stash the event so it can be triggered later.
-  installPromptEvent = event;
-  // Update the install UI to notify the user app can be installed
-  document.querySelector('#install-button').disabled = false;
-});
-
-btnInstall.addEventListener('click', () => {
-  // Update the install UI to remove the install button
-  document.querySelector('#install-button').disabled = true;
-  // Show the modal add to home screen dialog
-  installPromptEvent.prompt();
-  // Wait for the user to respond to the prompt
-  installPromptEvent.userChoice.then((choice) => {
-    if (choice.outcome === 'accepted') {
-      console.log('User accepted the A2HS prompt');
-    } else {
-      console.log('User dismissed the A2HS prompt');
-    }
-    // Clear the saved prompt since it can't be used again
-    installPromptEvent = null;
-  });
-});
